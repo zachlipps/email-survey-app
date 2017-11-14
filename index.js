a622a54453250a9e1abe215b526c998f3607dd44
Zach Lipps <zachlipps@Zachs-MBP.lan>
2017-11-14 11:41:38 -0800

    add config and keys

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
+import { reach } from '../../../Library/Caches/typescript/2.6/node_modules/@types/hoek';
+
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth2').Strategy;
+
 const app = express();
+
+passport.use(new GoogleStrategy());
 
 app.get('/', (req, res) => {
   res.send({ bye: 'buddy' });
