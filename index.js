eaa58853b4f5720f6d75fc700cc9dbca05e2d80a
Zach Lipps <zachlipps@Zachs-MBP.lan>
2017-11-14 13:43:08 -0800

    set up auth callback

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
-import { reach } from '../../../Library/Caches/typescript/2.6/node_modules/@types/hoek';
-
 const express = require('express');
 const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth2').Strategy;
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 
 const app = express();
 
-passport.use(new GoogleStrategy());
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback'
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log(accessToken);
+    }
+  )
+);
+
+app.get(
+  '/auth/google',
+  passport.authenticate('google', { scope: ['profile', 'email'] })
+);
+
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 app.get('/', (req, res) => {
   res.send({ bye: 'buddy' });
